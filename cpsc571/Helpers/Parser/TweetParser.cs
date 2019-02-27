6b86f3ff15b272c76f4b5b8873801b2cf7e8c72e
Nathan Geist <nathan@thinkgeist.com>
2019-02-26 21:04:07 -0700

    Additions to TweetParser to make it more strict.
    Still needs to filter out base word/phrase, possibly more work on what words to allow.

--- cpsc571/Helpers/Parser/TweetParser.cs
+++ cpsc571/Helpers/Parser/TweetParser.cs
@@ -2,6 +2,7 @@
 using OpenNLP.Tools.PosTagger;
 using OpenNLP.Tools.SentenceDetect;
 using OpenNLP.Tools.Tokenize;
+using System.Text.RegularExpressions;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,19 +21,30 @@ namespace cpsc571.Helpers
         private String SD_MODEL_PATH = Path.Combine(HttpRuntime.BinDirectory, @"Helpers\Parser\Models\EnglishSD.nbin");
         private String TOK_MODEL_PATH = Path.Combine(HttpRuntime.BinDirectory, @"Helpers\Parser\Models\EnglishTok.nbin");
         private String TAG_DICT_PATH = Path.Combine(HttpRuntime.BinDirectory, @"Helpers\Parser\Models\Parser\tagdict");
-        private String[] ALLOWED_TAGS = { "JJ", "JJR", "JJS", "MD", "NN", "NNS", "NNP", "NNPS" };
+        // Removed "VBN", "VBP", "VBZ"
+        private String[] ALLOWED_TAGS = { "CD", "JJ", "JJR", "JJS", "MD", "NN", "NNS", "NNP", "NNPS", "RB", "RBR", "RBS", "UH", "VB", "VBD", "VBG"};
+        private String pattern = @"[^0-9a-zA-Z\s']+";
 
         public List<String> ParseTweet(String tweetText)
         {
             List<String> result = new List<string>();
+
+            tweetText = tweetText.Replace("i ", "I ");
             EnglishMaximumEntropyTokenizer tokenizer = new EnglishMaximumEntropyTokenizer(TOK_MODEL_PATH);
             string[] tokens = tokenizer.Tokenize(tweetText);
             var posTagger = new EnglishMaximumEntropyPosTagger(POS_MODEL_PATH, TAG_DICT_PATH);
             string[] wordTags = posTagger.Tag(tokens);
             for(int i=0; i<tokens.Length; i++)
             {
-                if(ALLOWED_TAGS.Contains(wordTags[i])){
-                    result.Add(tokens[i]);
+                if (tokens[i].StartsWith("@"))
+                    continue;
+                else if (Uri.IsWellFormedUriString(tokens[i], UriKind.Absolute))
+                    continue;
+                else
+                {
+                    String token = Regex.Replace(tokens[i], pattern, "");
+                    if (ALLOWED_TAGS.Contains(wordTags[i]) && token.Length > 0)
+                        result.Add(token);
                 }
             }
             return result;
